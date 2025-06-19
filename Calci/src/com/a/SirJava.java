c1c4f77b7f4fe527d852136cf3093c452bdae8c7
srikanth <mankalisrikanth2@gmail.com>
2025-06-19 17:01:59 +0530

    project committed

---
+++ Calci/src/com/a/SirJava.java
@@ -0,0 +1,13 @@
+package com.a;
+
+public class SirJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1="Afroz";
+		System.out.println("hi iam java sir "+str1);
+	
+	}
+
+}
